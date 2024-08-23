d53e546c16ee1ddb86a728f42c78819b2914fe93
Nauman Mithani <nmithani1@gmail.com>
2024-08-22 23:00:20 -0700

    Start of setup of req. mocks
    Quick save
    
    Playwright equivalent for Cy test of project cards
    
    Issue list spec. failing
    
    passing
    
    Test "renders the issue" passes

---
+++ tests/issue-list.spec.ts
@@ -0,0 +1,42 @@
+// modules
+import { expect, Locator, test } from "@playwright/test";
+
+// files
+import { capitalize } from "lodash";
+import mockIssuesPageOne from "./fixtures/issues-page-1.json";
+
+test.describe("Issue list", () => {
+  const endpoint = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/dashboard/issues?page=1");
+    // setup request mocks
+    await page.route(`${endpoint}/issues?page=1`, async (route) => {
+      await route.fulfill({
+        body: JSON.stringify(mockIssuesPageOne),
+      });
+    });
+  });
+
+  test.describe("under 'Desktop' resolution", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.setViewportSize({ width: 1025, height: 900 });
+    });
+
+    test("renders the issues", async ({ page }) => {
+      for (const {
+        id,
+        attributes: { name, level, message },
+      } of mockIssuesPageOne.data) {
+        const issueRow: Locator = page.getByTestId(`issue-${id}`);
+        await expect(issueRow.getByTestId("error-name")).toContainText(name);
+        await expect(issueRow.getByTestId("badge-level")).toContainText(
+          capitalize(level),
+        );
+        await expect(issueRow.getByTestId("error-message")).toContainText(
+          message,
+        );
+      }
+    });
+  });
+});
